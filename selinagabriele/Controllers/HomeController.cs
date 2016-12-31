245ded8cb6a4394cf296b11770c3f2807bda1a48
selinagabes <gabrieleselina@gmail.com>
2016-12-30 20:30:24 -0500

    Initial Commit

---
+++ selinagabriele/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace selinagabriele.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title="Selina Gabriele";
+return View();
+        }
+    }
+}
