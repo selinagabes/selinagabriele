0b185ac968770b619fa6b6e83ac34709f8ac282a
selinagabes <gabrieleselina@gmail.com>
2017-01-05 23:28:33 -0500

    #master about page added, modal for contact popping up:

--- selinagabriele/Controllers/HomeController.cs
+++ selinagabriele/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using selinagabriele.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -15,8 +17,55 @@ namespace selinagabriele.Controllers
         }
         public ActionResult About()
         {
-            
+            ViewBag.Title = "Selina Gabriele";
             return View();
+        }
+       
+        public ActionResult Contact()
+        {
+            ContactViewModel contact = new ContactViewModel();
+
+            return PartialView("_ContactPartial",contact);
+        }
+        [HttpPost]
+        public ActionResult Contact(ContactViewModel contact)
+        {
+           
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    MailMessage message = new MailMessage();
+                    message.From = new MailAddress(contact.Email);//Email which you are getting 
+                                                         //from contact us page 
+                    message.To.Add("gabrieleselina@gmail.com");//Where mail will be sent 
+                    message.Subject = contact.Subject;
+                    message.Body = contact.Message;
+                    SmtpClient smtp = new SmtpClient();
+
+                    smtp.Host = "smtp.gmail.com";
+
+                    smtp.Port = 587;
+
+                    smtp.Credentials = new System.Net.NetworkCredential
+                    ("gabrieleselina@gmail.com", "Josef//95");
+
+                    smtp.EnableSsl = true;
+
+                    smtp.Send(message);
+
+                    ModelState.Clear();
+                    ViewBag.Message = "Thank you for Contacting us ";
+                }
+                catch (Exception ex)
+                {
+                    ModelState.Clear();
+                    ViewBag.Message = $" Sorry we are facing Problem here {ex.Message}";
+                }
+            }
+
+            return View();
+            return PartialView(contact);
         }
     }
 }
---
+++ selinagabriele/Models/ContactViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace selinagabriele.Models
+{
+    public class ContactViewModel
+    {
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        [Required]
+        public string Message { get; set; }
+    }
+}
